7b2a89289a35ad686e801630fe8b616c3891109a
Dvir <dvir199710@gmail.com>
2022-04-27 17:48:05 +0300

    final 1

--- Makefile
+++ Makefile
@@ -3,7 +3,7 @@ CXX=gcc
 
 all: main1
 main1: main1.o 
-	$(CXX) main1.o -o main1
+	$(CXX) main1.o -o main1 -lpthread
 main1.o: main1.c
 	$(CXX) -c main1.c
 clean :
--- main1.c
+++ main1.c
@@ -1,7 +1,16 @@
 // A C program to demonstrate linked list based implementation of queue
 #include <stdio.h>
 #include <stdlib.h>
+#include <pthread.h>
+#include <unistd.h>
 
+// Declaration of thread condition variable
+pthread_cond_t cond1 = PTHREAD_COND_INITIALIZER;
+ 
+// declaring mutex
+pthread_mutex_t lock = PTHREAD_MUTEX_INITIALIZER;
+ 
+int done = 1;
 // A linked list (LL) node to store a queue entry
 typedef struct QNode {
 	void* key;
@@ -24,15 +33,21 @@ pQNode newNode(void* k)
 }
 
 // A utility function to create an empty queue
-pQueue createQueue()
+pQueue createQ()
 {
 	pQueue q = (pQueue)malloc(sizeof(struct Queue));
 	q->front = q->rear = NULL;
 	return q;
 }
-
+void destoryQ(pQueue q){
+    while (q->front !=NULL)
+    {
+        deQ(q);
+    }
+    free(q); 
+}
 // The function to add a key k to q
-void enQueue(pQueue q, int k)
+void enQ(pQueue q, int k)
 {
 	// Create a new LL node
 	pQNode temp = newNode(k);
@@ -40,6 +55,8 @@ void enQueue(pQueue q, int k)
 	// If queue is empty, then new node is front and rear both
 	if (q->rear == NULL) {
 		q->front = q->rear = temp;
+        printf("Signaling condition variable cond1\n");
+        pthread_cond_signal(&cond1);
 		return;
 	}
 
@@ -49,36 +66,48 @@ void enQueue(pQueue q, int k)
 }
 
 // Function to remove a key from given queue q
-void deQueue(pQueue q)
+void deQ(pQueue q)
 {
+     pthread_mutex_lock(&lock);
 	// If queue is empty, return NULL.
-	if (q->front == NULL)
-		return;
-
+    if (q->front == NULL) {
+ 
+        // let's wait on condition variable cond1
+        printf("Waiting on condition variable cond1\n");
+        pthread_cond_wait(&cond1, &lock);
+    }
 	// Store previous front and move front one node ahead
 	pQNode temp = q->front;
-
 	q->front = q->front->next;
-
 	// If front becomes NULL, then change rear also as NULL
 	if (q->front == NULL)
 		q->rear = NULL;
-
 	free(temp);
+    pthread_mutex_unlock(&lock);
 }
 
 // Driver Program to test anove functions
 int main()
 {
-	pQueue q = createQueue();
-	enQueue(q, 10);
-	enQueue(q, 20);
-	deQueue(q);
-	deQueue(q);
-	enQueue(q, 30);
-	enQueue(q, 40);
-	enQueue(q, 50);
-	deQueue(q);
+	pQueue q = createQ();
+    pthread_t tid1, tid2;
+    int x = 5;
+    void * p[2] = {q, &x};
+    pthread_create(&tid1, NULL, deQ, q);
+    sleep(5);
+    pthread_create(&tid2, NULL, enQ, p);
+ 
+    // wait for the completion of thread 2
+    pthread_join(tid2, NULL);
+
+	enQ(q, 10);
+	enQ(q, 20);
+	deQ(q);
+	deQ(q);
+	enQ(q, 30);
+	enQ(q, 40);
+	enQ(q, 50);
+	deQ(q);
 	printf("Queue Front : %d \n", q->front->key);
 	printf("Queue Rear : %d\n", q->rear->key);
 	return 0;
